73703809ce894511beb02d33807608cf75cc6336
wkodate <n9lfcth9@gmail.com>
2013-01-12 16:45:39 +0900

    add xvideos

--- src/com/example/unisen/SubActivity.java
+++ src/com/example/unisen/SubActivity.java
@@ -17,6 +17,7 @@ public class SubActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_sub, menu);
 		return true;
+		//xvideos
 	}
 
 }
