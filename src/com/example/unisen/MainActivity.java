244c84bafba831d724c3e2d923799fa906a44d39
wkodate <n9lfcth9@gmail.com>
2013-01-12 18:37:20 +0900

    create start page. add countActivity and RankActivity

--- src/com/example/unisen/MainActivity.java
+++ src/com/example/unisen/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,17 +13,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+        
+        Button button_start = (Button)findViewById(R.id.start_id);
+        Button button_rank = (Button)findViewById(R.id.rank_id);
+        
+        button_start.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view) {
+        		Intent intent = new Intent(MainActivity.this, CountActivity.class);
+        		startActivity(intent);
+        		finish();
+        	}
+        });    
+        
+        button_rank.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view) {
+        		Intent intent = new Intent(MainActivity.this, RankActivity.class);
+        		startActivity(intent);
+        		finish();
+        	}
+        });    
     }
     
-    public void onTestClick(View view) {
-    	Intent intent = new Intent(this, SubActivity.class);
-    	startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
