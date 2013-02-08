ca032b5946aae397583a6df1ad19c4ccb55b696e
HBWandEoW <hbwandeow@gmail.com>
2013-02-09 02:45:50 +0900

    test3

--- src/com/example/unisen/ResultActivity.java
+++ src/com/example/unisen/ResultActivity.java
@@ -1,65 +1,54 @@
 package com.example.unisen;
 
-import twitter4j.TwitterException;
-import twitter4j.auth.OAuthAuthorization;
-import twitter4j.auth.RequestToken;
-import twitter4j.conf.ConfigurationContext;
-import twitter4j.conf.Configuration;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends Activity {
 
-	public static RequestToken _req = null;
-	public static OAuthAuthorization _oauth = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_result);
+
+		// time表示
+		Intent intent = getIntent();
+		double ResultTime = intent.getLongExtra("ResultTime", 10);
 		
-		Button homebutton = (Button)findViewById(R.id.homebutton_id);
-        homebutton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                        Intent intent = new Intent(ResultActivity.this, HomeActivity.class);
-                        startActivity(intent);
-                        finish();
-                }           
-        });         
-        Button twbutton = (Button)findViewById(R.id.tweetbutton_id);
-        twbutton.setOnClickListener(new OnClickListener() {
-        	
-        	@Override
-        	public void onClick(View v) {
-        		executeOauth();
-        		
-        	}
-        });
-	}
+		// set TextView
+		TextView result_time = (TextView) findViewById(R.id.result_time);
+		ResultTime = ResultTime / 1000;
+		result_time.setText(String.valueOf(ResultTime));
+
+		// 日付表示
+		//TextView date = (TextView) findViewById(R.id.date);
+		//date.setText(String.valueOf(""));
+
+		// Button
+		Button homeButton = (Button) findViewById(R.id.homebutton_id);
+		homeButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(ResultActivity.this,
+						HomeActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
 	
-	private void executeOauth() {
-		//Confファイル読み込み
-		Configuration conf = ConfigurationContext.getInstance();
+		// DBに登録
+		 ContentResolver contentResolver = getContentResolver();
+		 ContentValues contentValues = new ContentValues();
+		 contentValues.put("average", ResultTime);
+		 contentValues.put("date", System.currentTimeMillis());
+		 contentResolver.insert(Ranks.CONTENT_URI, contentValues);
 		
-		//Oauath認証オブジェクト生成
-		_oauth = new OAuthAuthorization(conf);
-		//Oauth認証オブジェクトにconsumerKeyとocnsumeSecretを設定
-		_oauth.setOAuthConsumer("a","b");
-		//アプリ認証オブジェクト作成
-		try {
-			_req = _oauth.getOAuthRequestToken("Callback://CallBackActivity");
-		} catch (TwitterException e) {
-			e.printStackTrace();
-		}
-		String _uri;
-		_uri = _req.getAuthorizationURL();
-		startActivityForResult(new Intent(Intent.ACTION_VIEW, Uri.parse(_uri)), 0);
-	
 	}
 
 	@Override
